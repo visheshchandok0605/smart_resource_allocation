3d5ed53e72bb6e27d676332bd4912b94695f1730
visheshchandok0605 <vishesh.chandok@joshsoftware.com>
2026-02-05 12:46:46 +0530

    Security: Move SMTP credentials to environment variables

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'vishesh.chandok@joshsoftware.com',
-    password:             'bzgj pcko ihmh eabd',
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
